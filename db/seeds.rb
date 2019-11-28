302c71ee57c5f080ac8d51e87cd6acf4dc246f2e
Kimberly Zhu <kimberly.zhu.2000@gmail.com>
2019-11-27 19:06:21 -0800

    reverted attr encryption

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-teachers = [{:encrypted_first_name => "Janani", :encrypted_last_name => "Vijaykumar", :encrypted_email => "janani_vijaykumar@berkeley.edu", :encrypted_school_name => "Monta Vista High School", 
-:encrypted_city => "Cupertino", :encrypted_state => "California", :encrypted_website => "https://mvhs.fuhsd.org", :encrypted_course => "I am teaching BJC as an AP CS Principles course.", 
-:encrypted_snap => "N/A", :encrypted_other => "N/A"},
-{:encrypted_first_name => "Kimberly", :encrypted_last_name => "Zhu", :encrypted_email => "kpzhu@berkeley.edu", :encrypted_school_name => "Cupertino High School",
-:encrypted_city => "Cupertino", :encrypted_state => "California", :encrypted_website => "https://chs.fuhsd.org", :encrypted_course => "I am teaching BJC but not as an AP CS Principles course.",
-:encrypted_snap => "N/A", :encrypted_other => "N/A"},
-{:encrypted_first_name => "Dalton", :encrypted_last_name => "Surprenant", :encrypted_email => "daltons@berkeley.edu", :encrypted_school_name => "San Diego High School",
-:encrypted_city => "San Diego", :encrypted_state => "California", :encrypted_website => "https://sdhs.org", :encrypted_course => "I am using BJC as a resource, but not teaching with it.",
-:encrypted_snap => "N/A", :encrypted_other => "N/A"},
-{:encrypted_first_name => "Ye", :encrypted_last_name => "Wang", :encrypted_email => "wangye@berkeley.edu", :encrypted_school_name => "New York High School",
-:encrypted_city => "New York", :encrypted_state => "New York", :encrypted_website => "https://nyhs.fuhsd.org", :encrypted_course => "I am a TEALS volunteer, and am teaching the BJC curriculum.",
-:encrypted_snap => "N/A", :encrypted_other => "N/A"},
-{:encrypted_first_name => "Zachary", :encrypted_last_name => "Chao", :email => "zachchao@berkeley.edu", :encrypted_school_name => "Santa Barbara High School",
-:cencrypted_cityity => "Santa Barbara", :encrypted_state => "California", :encrypted_website => "https://sbhs.org", :encrypted_course => "Other",
-:encrypted_snap => "N/A", :encrypted_other => "For a club"},
-{:encrypted_first_name => "Varun", :encrypted_last_name => "Murthy", :encrypted_email => "murthy@berkeley.edu", :encrypted_school_name => "Houston High School",
-:encrypted_city => "Houston", :encrypted_state => "Texas", :encrypted_website => "https://houstonTexas.org", :encrypted_course => "I am teaching BJC as an AP CS Principles course.",
-:encrypted_snap => "N/A", :encrypted_other => "N/A"}]
+teachers = [{:first_name => "Janani", :last_name => "Vijaykumar", :email => "janani_vijaykumar@berkeley.edu", :school_name => "Monta Vista High School",
+:city => "Cupertino", :state => "California", :website => "https://mvhs.fuhsd.org", :course => "I am teaching BJC as an AP CS Principles course.",
+:snap => "N/A", :other => "N/A"},
+
+{:first_name => "Kimberly", :last_name => "Zhu", :email => "kpzhu@berkeley.edu", :school_name => "Cupertino High School",
+:city => "Cupertino", :state => "California", :website => "https://chs.fuhsd.org", :course => "I am teaching BJC but not as an AP CS Principles course.",
+:snap => "N/A", :other => "N/A"},
+
+{:first_name => "Dalton", :last_name => "Surprenant", :email => "daltons@berkeley.edu", :school_name => "San Diego High School",
+:city => "San Diego", :state => "California", :website => "https://sdhs.org", :course => "I am using BJC as a resource, but not teaching with it.",
+:snap => "N/A", :other => "N/A"},
+
+{:first_name => "Ye", :last_name => "Wang", :email => "wangye@berkeley.edu", :school_name => "New York High School",
+:city => "New York", :state => "New York", :website => "https://nyhs.fuhsd.org", :course => "I am a TEALS volunteer, and am teaching the BJC curriculum.",
+:snap => "N/A", :other => "N/A"},
+
+
+{:first_name => "Zachary", :last_name => "Chao", :email => "zachchao@berkeley.edu", :school_name => "Santa Barbara High School",
+:city => "Santa Barbara", :state => "California", :website => "https://sbhs.org", :course => "Other",
+:snap => "N/A", :other => "For a club"},
+
+{:first_name => "Varun", :last_name => "Murthy", :email => "murthy@berkeley.edu", :school_name => "Houston High School",
+:city => "Houston", :state => "Texas", :website => "https://houstonTexas.org", :course => "I am teaching BJC as an AP CS Principles course.",
+:snap => "N/A", :other => "N/A"}
+
+]
 
 teachers.each do |teacher|
     Teacher.create!(teacher)
