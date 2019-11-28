79ca4c61c29d76ec70fdbab2c831baa1f0bac855
Kimberly Zhu <kimberly.zhu.2000@gmail.com>
2019-11-27 20:19:37 -0800

    completed form testing with cucumber feature and step definitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 teachers = [{:first_name => "Janani", :last_name => "Vijaykumar", :email => "janani_vijaykumar@berkeley.edu", :school_name => "Monta Vista High School",
-:city => "Cupertino", :state => "California", :website => "https://mvhs.fuhsd.org", :course => "I am teaching BJC as an AP CS Principles course.",
+:city => "Cupertino", :state => "CA", :website => "https://mvhs.fuhsd.org", :course => "I am teaching BJC as an AP CS Principles course.",
 :snap => "N/A", :other => "N/A"},
 
 {:first_name => "Kimberly", :last_name => "Zhu", :email => "kpzhu@berkeley.edu", :school_name => "Cupertino High School",
-:city => "Cupertino", :state => "California", :website => "https://chs.fuhsd.org", :course => "I am teaching BJC but not as an AP CS Principles course.",
+:city => "Cupertino", :state => "CA", :website => "https://chs.fuhsd.org", :course => "I am teaching BJC but not as an AP CS Principles course.",
 :snap => "N/A", :other => "N/A"},
 
 {:first_name => "Dalton", :last_name => "Surprenant", :email => "daltons@berkeley.edu", :school_name => "San Diego High School",
-:city => "San Diego", :state => "California", :website => "https://sdhs.org", :course => "I am using BJC as a resource, but not teaching with it.",
+:city => "San Diego", :state => "CA", :website => "https://sdhs.org", :course => "I am using BJC as a resource, but not teaching with it.",
 :snap => "N/A", :other => "N/A"},
 
 {:first_name => "Ye", :last_name => "Wang", :email => "wangye@berkeley.edu", :school_name => "New York High School",
-:city => "New York", :state => "New York", :website => "https://nyhs.fuhsd.org", :course => "I am a TEALS volunteer, and am teaching the BJC curriculum.",
+:city => "New York", :state => "NY", :website => "https://nyhs.fuhsd.org", :course => "I am a TEALS volunteer, and am teaching the BJC curriculum.",
 :snap => "N/A", :other => "N/A"},
 
 
 {:first_name => "Zachary", :last_name => "Chao", :email => "zachchao@berkeley.edu", :school_name => "Santa Barbara High School",
-:city => "Santa Barbara", :state => "California", :website => "https://sbhs.org", :course => "Other",
+:city => "Santa Barbara", :state => "CA", :website => "https://sbhs.org", :course => "Other",
 :snap => "N/A", :other => "For a club"},
 
 {:first_name => "Varun", :last_name => "Murthy", :email => "murthy@berkeley.edu", :school_name => "Houston High School",
-:city => "Houston", :state => "Texas", :website => "https://houstonTexas.org", :course => "I am teaching BJC as an AP CS Principles course.",
+:city => "Houston", :state => "TX", :website => "https://houstonTexas.org", :course => "I am teaching BJC as an AP CS Principles course.",
 :snap => "N/A", :other => "N/A"}
 
 ]
