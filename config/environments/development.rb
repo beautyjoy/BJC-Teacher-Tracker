77832b84cc2be872aae28c422f511ce5e5be77ae
Dalton <daltons@berkeley.edu>
2019-12-10 22:15:32 -0800

    Finished email functionality.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,20 +39,15 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.delivery_method = :sendmail
-
-  config.action_mailer.perform_deliveries = true
+  # Email settings
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'beautyandjoy2019@gmail.com'}
-
-  # SMTP settings for gmail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => 'email-smtp.us-west-2.amazonaws.com',
     :port                 => 587,
-    :user_name            => 'beautyandjoy2019@gmail.com',
-    :password             => 'bjc-teacher-tracker',
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    :user_name            => 'AKIA6JV7O2DSLSCZFINV',
+    :password             => Figaro.env.email_password,
+    :authentication       => :login,
+    :enable_starttls_auto => true,
   }
 end
