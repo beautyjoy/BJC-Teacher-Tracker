aa87d4083cdb6ad2f7e7edfadde5f855fa3ccd3f
Michael Ball <cycomachead@gmail.com>
2019-12-24 21:17:08 -0800

    Update specs to match new form fields

--- features/step_definitions/form_steps.rb
+++ features/step_definitions/form_steps.rb
@@ -1,7 +1,13 @@
+STATUS_FIELD = "What's your current status?"
+
 Given /^I enter my "(.*)" as "(.*)"$/ do |field_name, input|
     fill_in(field_name, with: input)
 end
-  
+
+Given /^I set my status as "(.*)"$/ do |input|
+    select(input, from: STATUS_FIELD)
+end
+
 Then("I see a confirmation {string}") do |string|
-    page.should have_content "Thanks for signing up!"   
+    page.should have_content "Thanks for signing up!"
 end
