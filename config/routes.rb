79229654907129694ae6a9f87933fdbb89ad5a3e
Michael Ball <cycomachead@gmail.com>
2019-12-24 21:55:49 -0800

    Update Routing (add dashboard, use ")

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+  mount JasmineRails::Engine => "/specs" if defined?(JasmineRails)
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -8,27 +8,29 @@ Rails.application.routes.draw do
   resources :teachers
   resources :admins
 
-  root to: 'main#index'
+  root to: "main#index"
 
   # The line below would be unnecessary since we use Google.
   # sessions#new could be left as an empty Ruby function.
   # We just need to define a "new" view to prompt for user name,
   # and password.
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  post   '/logout',  to: 'main#logout'
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
+  post   "/logout",  to: "main#logout"
 
   # Routes for Google authentication, note that these need to be
   # here for ominauth middleware whose route is /auth/google_oauth2,
   # which is not specified in this file, (because the middleware did it).
-  get 'auth/:provider/callback', to: 'sessions#googleAuth'
-  get 'auth/failure', to: redirect('/')
+  get "auth/:provider/callback", to: "sessions#googleAuth"
+  get "auth/failure", to: redirect("/")
 
   # Route for validating forms as an admin
-  post '/admin/forms/validate/:id', to: 'teachers#validate', as: "validate"
-  post '/admin/forms/delete/:id', to: 'teachers#delete', as: "delete"
+  # TODO: #21 - move to teachers/:id/...
+  post "/admin/forms/validate/:id", to: "teachers#validate", as: "validate"
+  post "/admin/forms/delete/:id", to: "teachers#delete", as: "delete"
 
   # Route for displaying all teacher information
-  get '/all', to: 'teachers#all', as: "all"
+  get "/all", to: "teachers#all", as: "all"
+  get "/dashboard", to "main#dashboard", as: "dashboard"
 end
