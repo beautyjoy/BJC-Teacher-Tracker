d0eec1a581927759f3c5b47bfac4bc18359d72db
Michael Ball <cycomachead@gmail.com>
2020-01-14 11:08:52 -0800

    Handle case when Google cannot be reached

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,10 +1,15 @@
-class School < ActiveRecord::Base
+# frozen_string_literal: true
+require 'uri'
+class School < ApplicationRecord
   validates :name, :city, :state, :website, presence: true
   before_save :grab_lat_lng
 
   has_many :teachers
 
   scope :validated, -> { where("num_validated_teachers > 0") }
+
+  MAPS_API_KEY = "AIzaSyC7jyOFHSorVb256ZEwvvyprp2KPjxKTPw"
+  GOOGLE_MAPS = "https://maps.googleapis.com/maps/api/geocode/"
 
   def website
     prefix_url(self[:website])
@@ -21,11 +26,13 @@ class School < ActiveRecord::Base
     url.match(/^https?:/) ? url : "https://#{url}"
   end
 
+  # TODO: URL encode this.
+  def maps_api_location
+    "#{self.city}+#{self.state}".sub(" ", "+")
+  end
+
   def grab_lat_lng
-    google_key = "AIzaSyC7jyOFHSorVb256ZEwvvyprp2KPjxKTPw"
-    location = self.city + " " + self.state
-    location.sub! " ", "+"
-    url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % [location, google_key]
+    url = "#{GOOGLE_MAPS}?json?address=#{maps_api_location}key=#{MAPS_API_KEY}"
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -35,5 +42,7 @@ class School < ActiveRecord::Base
     data = JSON.parse(res.body)
     self.lat = data["results"][0]["geometry"]["location"]["lat"]
     self.lng = data["results"][0]["geometry"]["location"]["lng"]
+  rescue
+    Rails.logger.warn("Could not set coordinates for school #{self.name}")
   end
 end
