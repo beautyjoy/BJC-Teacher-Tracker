a081ee23bcb9f87c796b9ecfc15d027a1bcd5be0
Michael Ball <cycomachead@gmail.com>
2020-02-17 17:03:35 -0800

    Add status to teachers

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,50 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191224104817) do
+ActiveRecord::Schema.define(version: 2020_02_18_010024) do
 
   create_table "admins", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "encrypted_google_token"
-    t.string   "encrypted_google_token_iv"
-    t.string   "encrypted_google_refresh_token"
-    t.string   "encrypted_google_refresh_token_iv"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "encrypted_google_token"
+    t.string "encrypted_google_token_iv"
+    t.string "encrypted_google_refresh_token"
+    t.string "encrypted_google_refresh_token_iv"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["email", "first_name"], name: "index_admins_on_emails_and_first_name"
   end
-
-  add_index "admins", ["email", "first_name"], name: "index_admins_on_emails_and_first_name"
 
   create_table "schools", force: :cascade do |t|
-    t.string   "name"
-    t.string   "city"
-    t.string   "state"
-    t.string   "website"
-    t.float    "lat"
-    t.float    "lng"
-    t.integer  "num_validated_teachers", default: 0
-    t.integer  "teachers_count",         default: 0
-    t.datetime "created_at",             default: '2019-12-11 07:35:22'
-    t.datetime "updated_at",             default: '2019-12-11 07:35:22'
+    t.string "name"
+    t.string "city"
+    t.string "state"
+    t.string "website"
+    t.float "lat"
+    t.float "lng"
+    t.integer "num_validated_teachers", default: 0
+    t.integer "teachers_count", default: 0
+    t.datetime "created_at", default: "2019-12-11 07:35:22"
+    t.datetime "updated_at", default: "2019-12-11 07:35:22"
+    t.index ["name", "city", "website"], name: "index_schools_on_name_city_and_website"
   end
-
-  add_index "schools", ["name", "city", "website"], name: "index_schools_on_name_city_and_website"
 
   create_table "teachers", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "course"
-    t.string   "snap"
-    t.string   "other"
-    t.integer  "school_id"
-    t.boolean  "validated"
-    t.datetime "created_at", default: '2019-12-11 07:35:22'
-    t.datetime "updated_at", default: '2019-12-11 07:35:22'
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "course"
+    t.string "snap"
+    t.string "other"
+    t.integer "school_id"
+    t.boolean "validated"
+    t.datetime "created_at", default: "2019-12-11 07:35:22"
+    t.datetime "updated_at", default: "2019-12-11 07:35:22"
+    t.integer "status"
+    t.index ["email", "first_name"], name: "index_teachers_on_email_and_first_name"
   end
-
-  add_index "teachers", ["email", "first_name"], name: "index_teachers_on_email_and_first_name"
 
 end
