db9f4a9a06e2f5989e3fd03c48ee400e53b11a63
Michael Ball <cycomachead@gmail.com>
2020-02-17 19:24:27 -0800

    CC lauren on verification emials

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -5,7 +5,9 @@ class TeacherMailer < ApplicationMailer
 
   def welcome_email(teacher)
     @teacher = teacher
-    mail to: @teacher.email, subject: 'Welcome to the Beauty and Joy of Computing!'
+    mail to: @teacher.email,
+         cc: CONTACT_EMAIL,
+         subject: 'Welcome to the Beauty and Joy of Computing!'
   end
 
   def form_submission(teacher)
