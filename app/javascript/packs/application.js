8afc80459d6b84192dd6d951ea7c810f6cda1546
Michael Ball <cycomachead@gmail.com>
2020-10-29 13:30:48 -0700

    Fix font awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,8 @@ import 'jquery-ujs/src/rails'
 import 'bootstrap';
 import 'popper.js';
 
+import "@fortawesome/fontawesome-free/js/all";
+
 import "datatables.net-bs4";
 import "datatables.net-buttons-bs4";
 import  'datatables.net-buttons/js/buttons.html5.js';
