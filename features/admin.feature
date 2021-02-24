466e97920e88810756f0681202975028eb35baf1
Tommy Wei <tommywei@google.com>
2021-02-24 10:44:10 -0800

    fix all the test, all passing

--- features/admin.feature
+++ features/admin.feature
@@ -6,29 +6,38 @@ Feature: basic admin functionality
 
 Scenario: Logging in as an admin
   Given I am on the BJC home page
+  Given I have an admin email
   And I follow "Admin Log In"
-  Then I can log in with Google as Admin
+  Then I can log in with Google
   And I should see "BJC Teacher Dashboard"
 
 # TODO: Add steps to check basic dashboard functions
 # A search field should be visible on the index pages
 # Check for a CSV button
 
-Scenario: Logging in as a non admin should not give admin access
+Scenario: Logging in as a teacher should be able to edit their info
   Given the following teachers exist:
-  | first_name | last_name | admin | email                          |
-  | Joseph     | Mamoa     | false | "testteacheruser@berkeley.edu" |
+  | first_name | last_name | admin | email                |
+  | Joseph     | Mamoa     | false | testteacher@berkeley.edu |
+  Given I have a teacher email
   Given I am on the BJC home page
   And I follow "Admin Log In"
-  Then I can log in with Google as Teacher
-  And I should see "Request Access to Teacher Materials"
+  Then I can log in with Google
+  And I should see "You can edit your information"
+
+Scenario: Logging in with random Google Account should fail
+  Given I have a random email
+  Given I am on the BJC home page
+  And I follow "Admin Log In"
+  Then I can log in with Google
+  And I should see "Please Submit a teacher request"
 
 Scenario: Logging in as an env-removed admin should not give admin access
   Given the following teachers exist:
-  | first_name | last_name | admin | email                          |
-  | Joseph     | Mamoa     | true | "testteacheruser@berkeley.edu" |
+  | first_name | last_name | admin | email                        |
+  | Joseph     | Mamoa     | true  | testteacheruser@berkeley.edu |
   Given I am on the BJC home page
   And I follow "Admin Log In"
   Then I can log in with Google as Teacher
-  And I should see "Request Access to Teacher Materials"
+  And I should see "Please Submit a teacher request"
 
--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,21 +1,62 @@
 require 'cucumber/rspec/doubles'
 
-Given /I am an admin user/ do
-  visit root_url
+Given /I have an admin email/ do
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+    provider: 'google_oauth2',
+    uid: '123545',
+    info: {
+      name: 'Admin User',
+      first_name: "Admin",
+      last_name: "User",
+      email: "testadminuser@berkeley.edu",
+      school: "UC Berkeley",
+    },
+    credentials: {
+      token: 'test_token'
+    }
+  })
 end
+
+Given /I have a teacher email/ do
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+    provider: 'google_oauth2',
+    uid: '123545',
+    info: {
+      name: 'Admin User',
+      first_name: "Admin",
+      last_name: "User",
+      email: "testteacher@berkeley.edu",
+      school: "UC Berkeley",
+    },
+    credentials: {
+      token: 'test_token'
+    }
+  })
+end
+
+Given /I have a random email/ do
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+    provider: 'google_oauth2',
+    uid: '123545',
+    info: {
+      name: 'Admin User',
+      first_name: "Admin",
+      last_name: "User",
+      email: "randomemail@gmail.com",
+      school: "UC Berkeley",
+    },
+    credentials: {
+      token: 'test_token'
+    }
+  })
+end
+
 
 # A wrapper around the omniauth link.
 # TODO: This should be adapted to take in a provider.
-Then /I can log in with Google as Admin/ do
+Then /I can log in with Google/ do
   allow(Teacher).to receive(:validate_auth).and_return(true)
-  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2_admin]
-  page.find('button', text: /.*Sign in/).click()
-end
-
-#Combine with the previous method if this works fine -- Steven 2/23
-Then /I can log in with Google as Teacher/ do
-  allow(Teacher).to receive(:validate_auth).and_return(true)
-  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2_teacher]
+  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
   page.find('button', text: /.*Sign in/).click()
 end
 
