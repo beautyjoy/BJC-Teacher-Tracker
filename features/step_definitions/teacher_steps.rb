09e307c41129af66938991f67cc4e67b4d243784
Steven Yu <steven.yu@berkeley.edu>
2021-03-09 19:36:25 -0800

    Direct school associations for testing

--- features/step_definitions/teacher_steps.rb
+++ features/step_definitions/teacher_steps.rb
@@ -1,5 +1,6 @@
 require 'cucumber/rspec/doubles'
 
+# Returns a OAuth2 token associated with email "testteacher@berkeley.edu"
 Given /I have a teacher email/ do
   OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
     provider: 'google_oauth2',
@@ -44,7 +45,20 @@ Given(/the following teachers exist/) do |teachers_table|
         teacher[key] = value
       end
     end
-    Teacher.create!(teacher)
+
+    # Extract extra parameter 'school'
+    school_name = teacher.delete("school")
+    
+    new_teacher = Teacher.create!(teacher)
+
+    #Create an association between teacher and school
+    if school_name != nil
+      school = School.find_by(name: school_name)
+      if school != nil
+        new_teacher.school = school
+        new_teacher.save!
+      end
+    end
   end
 end
 
