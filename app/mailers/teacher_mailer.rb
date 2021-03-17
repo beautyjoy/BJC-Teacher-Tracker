d5c09696014bff47d6213cd6784453ef75739c52
Christopher Hou <chris.hou@berkeley.edu>
2021-03-16 22:22:16 -0700

    Added framework email + testing

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class TeacherMailer < ApplicationMailer
-  CONTACT_EMAIL = 'Lauren Mock <lmock@berkeley.edu>'
+  CONTACT_EMAIL = 'Christopher Hou <chris.hou@berkeley.edu>' #'Lauren Mock <lmock@berkeley.edu>'
+  TEALS_CONTACT_EMAIL = 'Christopher Hou <chris.hou@berkeley.edu>' #CHANGE THIS LATER
   BJC_EMAIL = 'BJC <contact@bjc.berkeley.edu>'
   BJC_PASSWORD = Rails.application.secrets[:bjc_password]
   PIAZZA_PASSWORD = Rails.application.secrets[:piazza_password]
@@ -13,6 +14,17 @@ class TeacherMailer < ApplicationMailer
     mail to: @teacher.email_name,
          cc: CONTACT_EMAIL,
          subject: 'Welcome to The Beauty and Joy of Computing!'
+  end
+
+  def teals_confirmation_email(teacher)
+    @teacher = teacher
+    if !@teacher.status.nil? and @teacher.status.include? "TEALS"
+      @bjc_password = BJC_PASSWORD
+      @piazza_password = PIAZZA_PASSWORD
+      mail to: TEALS_CONTACT_EMAIL,
+           cc: CONTACT_EMAIL,
+           subject: 'TEALS Confirmation Email'
+    end
   end
 
   def form_submission(teacher)
