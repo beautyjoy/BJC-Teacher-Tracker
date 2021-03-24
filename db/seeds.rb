b826ee9dc58fbe6f84fbf91d48ec25bca76de1d8
Long Tong <longhtong@berkeley.edu>
2021-03-24 11:02:18 -0700

    using find_or_create

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ require_relative 'seed_data'
 
 Teacher.destroy_all
 
-SeedData.teachers.each {|teacher| Teacher.create!(teacher)}
+SeedData.teachers.each {|teacher| Teacher.find_or_create_by(teacher)}
