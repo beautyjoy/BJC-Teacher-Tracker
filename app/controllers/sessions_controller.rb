d39e86950142b25f0d484b3110a8c05d32f0880a
Christopher Hou <chris.hou@berkeley.edu>
2021-04-19 17:47:32 -0700

    Got Microsoft login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    redirect_to "/auth/google_oauth2"
+    redirect_to "/auth/microsoft_graph"
+    #redirect_to "/auth/google_oauth2"
   end
 
   def destroy
@@ -14,17 +15,38 @@ class SessionsController < ApplicationController
 
   def googleAuth
     # Manages the callback from Google
-    # Get access tokens from the google server
+    # Get access tokens from the Google server
     access_token = request.env["omniauth.auth"]
     if Teacher.validate_access_token(access_token)
       # Tell them to register.
       user = Teacher.user_from_omniauth(access_token)
-      # Access_token is used to authenticate request made from the rails application to the google server
+      # Access_token is used to authenticate request made from the rails application to the Google server
       user.google_token = access_token.credentials.token
       # Refresh_token to request new access_token
       # Note: Refresh_token is only sent once during the first request
       refresh_token = access_token.credentials.refresh_token
       user.google_refresh_token = refresh_token if refresh_token.present?
+      user.save!
+      log_in(user)
+      redirect_to root_path
+    else
+      redirect_to root_path, alert: "Please Submit a teacher request"
+    end
+  end
+
+  def microsoftAuth
+    # Manages the callback from Microsoft
+    # Get access tokens from the Microsoft server
+    access_token = request.env["omniauth.auth"]
+    if Teacher.validate_access_token(access_token)
+      # Tell them to register.
+      user = Teacher.user_from_omniauth(access_token)
+      # Access_token is used to authenticate request made from the rails application to the Microsoft server
+      user.microsoft_token = access_token.credentials.token
+      # Refresh_token to request new access_token
+      # Note: Refresh_token is only sent once during the first request
+      refresh_token = access_token.credentials.refresh_token
+      user.microsoft_refresh_token = refresh_token if refresh_token.present?
       user.save!
       log_in(user)
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,8 @@ Rails.application.routes.draw do
   # Routes for Google authentication, note that these need to be
   # here for ominauth middleware whose route is /auth/google_oauth2,
   # which is not specified in this file, (because the middleware did it).
-  match '/auth/:provider/callback', :to => 'auth#callback', :via => [:get, :post]
+  get "auth/google_oauth2/callback", to: "sessions#googleAuth"
+  get "auth/microsoft_graph/callback", to: "sessions#microsoftAuth"
   get "auth/failure", to: redirect("/")
 
   # Route for validating forms as an admin
