ce4a5722c4812bb7026667d82808b9b1d2168f6a
Christopher Hou <chris.hou@berkeley.edu>
2021-04-23 12:00:13 -0700

    Basic Snap framework

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,6 +22,9 @@ class SessionsController < ApplicationController
     when "microsoft_graph"
       user.microsoft_token = token
       user.microsoft_refresh_token = refresh_token if refresh_token.present?
+    when "discourse"
+      user.snap_token = token
+      user.snap_refresh_token = refresh_token if refresh_token.present?
     end
   end
 
