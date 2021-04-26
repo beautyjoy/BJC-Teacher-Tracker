658f25182105ec8369f6b6e428fdb224e17c6cde
Christopher Hou <chris.hou@berkeley.edu>
2021-04-25 22:49:12 -0700

    Updating to same as Snap

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,5 +1,7 @@
 require 'cucumber/rspec/doubles'
 
+LOGIN_SERVICE = { "Google" => :google_oauth2, "Microsoft" => :microsoft_graph, "Clever" => :clever }
+
 # Returns a OAuth2 token associated with email "testadminuser@berkeley.edu"
 Given /I have an admin email/ do
   OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
@@ -13,88 +15,43 @@ Given /I have an admin email/ do
       school: "UC Berkeley",
     },
     credentials: {
-      token: 'test_token'
+      token: 'test_token',
+      refresh_token: 'test_refresh_token'
     }
   })
 end
 
 # Returns a OAuth2 token associated with email "randomemail@gmail.com"
-Given /I have a non-admin, unregistered Google email/ do
-  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-    provider: 'google_oauth2',
+Given /I have a non-admin, unregistered (.*) email/ do |login|
+  service = LOGIN_SERVICE[login]
+  OmniAuth.config.mock_auth[service] = OmniAuth::AuthHash.new({
+    provider: service,
     uid: '123545',
     info: {
       name: 'Random User',
       first_name: "Random",
       last_name: "User",
-      email: "randomemail@gmail.com",
+      email: "randomemail@berkeley.edu",
       school: "UC Berkeley",
     },
     credentials: {
-      token: 'test_token'
-    }
-  })
-end
-
-# Returns a OAuth2 token associated with email "randomemail@gmail.com"
-Given /I have a non-admin, unregistered Microsoft email/ do
-  OmniAuth.config.mock_auth[:microsoft_graph] = OmniAuth::AuthHash.new({
-    provider: 'microsoft_graph',
-    uid: '123545',
-    info: {
-      name: 'Random User',
-      first_name: "Random",
-      last_name: "User",
-      email: "randomemail@microsoft.com",
-      school: "UC Berkeley",
-    },
-    credentials: {
-      token: 'test_token'
-    }
-  })
-end
-
-# Returns a OAuth2 token associated with email "randomemail@gmail.com"
-Given /I have a non-admin, unregistered Clever email/ do
-  OmniAuth.config.mock_auth[:clever] = OmniAuth::AuthHash.new({
-    provider: 'clever',
-    uid: '123545',
-    info: {
-      name: 'Random User',
-      first_name: "Random",
-      last_name: "User",
-      email: "randomemail@microsoft.com",
-      school: "UC Berkeley",
-    },
-    credentials: {
-      token: 'test_token'
+      token: 'test_token',
+      refresh_token: 'test_refresh_token'
     }
   })
 end
 
 # A wrapper around the omniauth link.
-# TODO: This should be adapted to take in a provider.
-Then /I can log in with Google/ do
+Then /I can log in with (.*)/ do |login|
+  service = LOGIN_SERVICE[login]
   allow(Teacher).to receive(:validate_auth).and_return(true)
-  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
-  page.find('button', text: /.*Sign in with Google/).click()
+  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[service]
+  page.find('button', text: /.*Sign in with #{login}/).click()
 end
 
 Then /I cannot log in with Google/ do
   OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
   page.find('button', text: /.*Sign in with Google/).click()
-end
-
-Then /I can log in with Microsoft/ do
-  allow(Teacher).to receive(:validate_auth).and_return(true)
-  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:microsoft_graph]
-  page.find('button', text: /.*Sign in with Microsoft/).click()
-end
-
-Then /I can log in with Clever/ do
-  allow(Teacher).to receive(:validate_auth).and_return(true)
-  Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:clever]
-  page.find('button', text: /.*Sign in with Clever/).click()
 end
 
 And /The TEALS contact email is stubbed/ do
