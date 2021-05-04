97a6d9cca10880c38185f60e0bf66642ea565e15
Michael Ball <cycomachead@gmail.com>
2021-05-03 21:14:44 -0700

    Fix seeds

--- db/seed_data.rb
+++ db/seed_data.rb
@@ -117,61 +117,15 @@ module SeedData
           {
               first_name: 'Michael',
               last_name: 'Ball',
-              email: 'michael.ball@berkeley.edu',
+              email: 'ball@berkeley.edu',
               admin: true,
               status: 0,
               application_status: 'Validated'
           },
-
           {
-              first_name: 'Christopher',
-              last_name: 'Hou',
-              email: 'chris.hou@berkeley.edu',
-              admin: true,
-              status: 0,
-              application_status: 'Validated'
-          },
-
-          {
-              first_name: 'Justin',
-              last_name: 'Phyo',
-              email: 'justinphyo@berkeley.edu',
-              admin: true,
-              status: 0,
-              application_status: 'Validated'
-          },
-
-          {
-              first_name: 'Richard',
-              last_name: 'Liu',
-              email: 'richardliu2000@berkeley.edu',
-              admin: true,
-              status: 0,
-              application_status: 'Validated'
-          },
-
-          {
-              first_name: 'Tommy',
-              last_name: 'Wei',
-              email: 'tommywei110@berkeley.edu',
-              admin: true,
-              status: 0,
-              application_status: 'Validated'
-          },
-
-          {
-              first_name: 'Steven',
-              last_name: 'Yu',
-              email: 'steven.yu@berkeley.edu',
-              admin: true,
-              status: 0,
-              application_status: 'Validated'
-          },
-
-          {
-              first_name: 'Long',
-              last_name: 'Tong',
-              email: 'longhtong@berkeley.edu',
+              first_name: 'Lauren',
+              last_name: 'Mock',
+              email: 'lmock@berkeley.edu',
               admin: true,
               status: 0,
               application_status: 'Validated'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 require_relative 'seed_data'
 
-Teacher.where(admin: true).destroy_all
-EmailTemplate.destroy_all
+SeedData.emails.each { |email| EmailTemplate.find_or_create_by(email) }
 
-SeedData.emails.each {|email| EmailTemplate.find_or_create_by(email)}
-SeedData.teachers.each {|teacher| Teacher.find_or_create_by(teacher)}
+SeedData.teachers.each { |teacher| Teacher.find_or_create_by(teacher) }
