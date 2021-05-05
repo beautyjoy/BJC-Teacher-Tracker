ba46441eeac58e23a6bf19dd2700ef2ca9f28399
Michael Ball <cycomachead@gmail.com>
2021-05-05 12:56:30 -0700

    Make seeding more robust

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@ require_relative 'seed_data'
 
 SeedData.emails.each { |email| EmailTemplate.find_or_create_by(email) }
 
-SeedData.teachers.each { |teacher| Teacher.find_or_create_by(teacher) }
+SeedData.teachers.each do |teacher|
+  Teacher.find_or_create_by(email: teacher[:email]).update_attributes(teacher)
+end
