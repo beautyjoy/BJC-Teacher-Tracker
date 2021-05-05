431efc89827dfa4412bfc142b5971e9dd5780364
Michael Ball <cycomachead@gmail.com>
2021-05-05 13:57:42 -0700

    TinyMCE works via Webpack. Cleanup HTML on edit view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,24 @@ import 'popper.js';
 import "@fortawesome/fontawesome-free/js/all";
 
 // TODO: These really out to be an 'admin' pack.
+// Import TinyMCE
 import 'tinymce';
+
+// Default icons are required for TinyMCE 5.3 or above
+import 'tinymce/icons/default';
+
+import 'tinymce/themes/silver';
+// Any plugins you want to use has to be imported
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/paste';
+import 'tinymce/plugins/table';
+
+// TinyMCE Skins. These need to be manually loaded.
+import 'tinymce/skins/ui/oxide/skin.min.css';
+import 'tinymce/skins/content/default/content.css';
+import 'tinymce/skins/ui/oxide/content.min.css';
+
 import 'datatables.net';
 import "datatables.net-bs4";
 import "datatables.net-buttons-bs4";
