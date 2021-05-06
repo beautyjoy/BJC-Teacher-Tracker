fd1b37953b46a3a47308d333d02b0ded40f0e861
Michael Ball <cycomachead@gmail.com>
2021-05-06 02:26:07 -0700

    Config for tinymce

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,22 @@
 const webpack = require('webpack')
 const { environment } = require('@rails/webpacker')
+const env = require('@rails/webpacker/package/env')
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
 }))
+
+// This is a hack!!
+// We need to exclude the tinymce CSS files from the CSS loader.
+let loader;
+for (num in environment.loaders) {
+  loader = environment.loaders[num];
+  if (loader.key === 'css') {
+    loader.value.exclude = /.*skin.*/;
+  }
+}
 
 environment.config.merge({
   module: {
@@ -38,4 +49,5 @@ environment.config.merge({
   }
 })
 
+console.log(environment.loaders)
 module.exports = environment
