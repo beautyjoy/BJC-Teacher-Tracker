f279d744dab153b36b13f4a2b549f1268781ee72
Michael Ball <cycomachead@gmail.com>
2021-10-14 12:35:15 -0700

    Add firefox to browsers

--- features/support/browsers.rb
+++ features/support/browsers.rb
@@ -1,7 +1,11 @@
+# Original Source
 # https://gist.github.com/bbonamin/4b01be9ed5dd1bdaf909462ff4fdca95
 require 'capybara/rspec'
 require 'selenium/webdriver'
 require 'cucumber'
+
+
+### Google Chrome
 
 chrome_options = Selenium::WebDriver::Chrome::Options.new
 chrome_options.add_preference(:download, prompt_for_download: false,
@@ -10,13 +14,13 @@ chrome_options.add_preference(:download, prompt_for_download: false,
 chrome_options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  options.add_argument('--headless')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--window-size=1680,1050')
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-gpu')
+  chrome_options.add_argument('--window-size=1680,1050')
 
   driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
 
@@ -35,6 +39,23 @@ Capybara.register_driver :headless_chrome do |app|
   ###
 
   driver
+end
+
+#### Safari (macOS only.)
+
+
+### Firefox
+Capybara.register_driver :firefox do |app|
+  Capybara::Selenium::Driver.new(app, browser: :firefox)
+end
+
+Capybara.register_driver :headless_firefox do |app|
+  options = Selenium::WebDriver::Firefox::Options.new
+  options.headless! # added on https://github.com/SeleniumHQ/selenium/pull/4762
+
+  Capybara::Selenium::Driver.new app,
+    browser: :firefox,
+    options: options
 end
 
 Capybara.javascript_driver = ENV['GUI'] ? :chrome : :headless_chrome
