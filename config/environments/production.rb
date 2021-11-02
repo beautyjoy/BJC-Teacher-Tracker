e142e5d88a73b2cd113019474941317306d93145
Michael Ball <cycomachead@gmail.com>
2021-11-01 18:29:01 -0700

    log less stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,11 +26,6 @@ Rails.application.configure do
   # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = false
-
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
@@ -51,10 +46,13 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
+
+  # Don't log view information:
+  config.action_view.logger = nil
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
