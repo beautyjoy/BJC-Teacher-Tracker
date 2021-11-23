0f85a3eddfa69b403d9836a299f0925108f8ca13
Michael Ball <cycomachead@gmail.com>
2021-11-23 15:16:34 -0800

    Add simplecov-console to see test output in CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
+require 'simplecov-csv'
+require 'simplecov-console'
 SimpleCov.start 'rails' do
   command_name 'rspec'
+  enable_coverage :branch
+
+  # SimpleCov::Formatter::SimpleFormatter
   formatter SimpleCov::Formatter::MultiFormatter.new([
-    SimpleCov::Formatter::SimpleFormatter,
+    SimpleCov::Formatter::CSVFormatter,
+    SimpleCov::Formatter::Console,
     SimpleCov::Formatter::HTMLFormatter
   ])
 end
