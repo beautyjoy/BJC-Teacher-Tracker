76124221302f9f8e85c28dc66825d5c1b8e31883
Michael Ball <cycomachead@gmail.com>
2022-01-08 17:34:57 -0500

    Fix specs: Changing school should not reset a teachers status

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -98,7 +98,7 @@ class Teacher < ApplicationRecord
   after_commit :update_school_counts
 
   def reset_validation_status
-    return if application_status_changed?
+    return if application_status_changed? or school_id_changed?
     if denied?
       pending!
     end
