d54ed63c9c653fec06b4ead7f76552a447f5f24e
Michael Ball <cycomachead@gmail.com>
2022-01-09 01:35:38 -0500

    Replace update_attributes with update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@ require_relative "seed_data"
 SeedData.emails.each { |email| EmailTemplate.find_or_create_by(email) }
 
 SeedData.teachers.each do |teacher|
-  Teacher.find_or_create_by(email: teacher[:email]).update_attributes(teacher)
+  Teacher.find_or_create_by(email: teacher[:email]).update(teacher)
 end
