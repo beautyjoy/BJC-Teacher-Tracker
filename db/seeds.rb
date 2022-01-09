47fd73b5f680cf135376528c68792a469709acd6
Michael Ball <cycomachead@gmail.com>
2022-01-09 15:27:25 -0500

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ require_relative "seed_data"
 
 SeedData.emails.each { |email| EmailTemplate.find_or_create_by(email) }
 
+SeedData.create_schools
+
 SeedData.teachers.each do |teacher|
   Teacher.find_or_create_by(email: teacher[:email]).update(teacher)
 end
