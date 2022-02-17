39cf6520105412bcbf6c64f6e2f8f433d4ee9ff6
Steve Gao <steve_gao@berkeley.edu>
2022-02-17 01:28:55 -0800

    fk3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,9 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "simplecov"
+require "simplecov-cobertura"
+
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 SimpleCov.start "rails"
 
 require "spec_helper"
