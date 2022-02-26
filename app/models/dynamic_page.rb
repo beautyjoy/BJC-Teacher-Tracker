76bfdab9b184a28739bdfd7583debbdedb921cb2
Hao Jie Zhou <haojiezhou@berkeley.edu>
2022-02-25 22:52:30 -0800

    Fixed rubocop

--- app/models/dynamic_page.rb
+++ app/models/dynamic_page.rb
@@ -4,5 +4,5 @@ class DynamicPage < ApplicationRecord
   def self.create_dynamic_page(dynamic_page_hash)
     if DynamicPage.find_by(slug: params[:slug]).blank?
       DynamicPage.create!(dynamic_page_hash)
-  end
+    end
 end
