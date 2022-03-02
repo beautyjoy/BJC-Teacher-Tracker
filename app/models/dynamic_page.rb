ad11c62e4f4b78f5415ccb6bf38458e5657af16c
Mitchell Wong <mitchell2001wong@gmail.com>
2022-03-01 16:16:38 -0800

    Deleted unnecessary code

--- app/models/dynamic_page.rb
+++ app/models/dynamic_page.rb
@@ -1,9 +1,3 @@
 # frozen_string_literal: true
 
-class DynamicPage < ApplicationRecord
-  def self.create_dynamic_page(dynamic_page_params)
-    if DynamicPage.find_by(slug: params[:slug]).blank?
-      DynamicPage.create!(dynamic_page_params)
-    end
-  end
-end
+class DynamicPage < ApplicationRecord end
