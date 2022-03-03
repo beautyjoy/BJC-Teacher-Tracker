8dcfe0e9f603a09e7dbcdb3965ceb04854dcfb26
Mitchell Wong <mitchell2001wong@gmail.com>
2022-03-02 16:17:55 -0800

    Rubocop fixes

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_03_02_022924) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,5 +85,4 @@ ActiveRecord::Schema.define(version: 2022_03_02_022924) do
     t.index ["snap"], name: "index_teachers_on_snap", unique: true, where: "((snap)::text <> ''::text)"
     t.index ["status"], name: "index_teachers_on_status"
   end
-
 end
