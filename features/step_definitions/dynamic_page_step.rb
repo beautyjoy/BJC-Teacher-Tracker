3f0fb318cd9bfa1223e0f296c67b4a995fb2b84b
Carlos <creyes37@berkeley.edu>
2022-03-12 19:23:34 -0800

    added test

--- features/step_definitions/dynamic_page_step.rb
+++ features/step_definitions/dynamic_page_step.rb
@@ -1,8 +1,7 @@
 When(/^I press "(.*)" button for "(.*)"$/)do |button, slug|
   Capybara.ignore_hidden_elements = false
-  elements = page.find('Delete', visible: :all)
   page = DynamicPage.find_by(slug: slug)
-  find_button(id: page.slug).click
+  find('td', text: 'Delete',visible:false).click_link('Delete')
   Capybara.ignore_hidden_elements = true
 end
 Given(/^I enter (?:my)? "(.*)" as "(.*)"$/) do |field_name, input|
