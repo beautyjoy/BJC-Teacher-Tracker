5bd8fbc5290714243f364dd0cacc74274d42fbe8
Carlos <creyes37@berkeley.edu>
2022-03-13 16:18:47 -0700

    this

--- features/step_definitions/dynamic_page_step.rb
+++ features/step_definitions/dynamic_page_step.rb
@@ -1,6 +1,6 @@
 When(/^I press "(.*)" button for "(.*)"$/)do |button, slug|
   Capybara.ignore_hidden_elements = false
   page = DynamicPage.find_by(slug: slug)
-  find_button(id: page.slug).click
+  find_button(button, id: slug).click
   Capybara.ignore_hidden_elements = true
 end
