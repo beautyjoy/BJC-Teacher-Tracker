08ee598c13ebd4a09d42fc573ee385decace4062
Carlos <creyes37@berkeley.edu>
2022-03-12 22:28:21 -0800

    padding changed

--- features/step_definitions/dynamic_page_step.rb
+++ features/step_definitions/dynamic_page_step.rb
@@ -1,9 +1,6 @@
 When(/^I press "(.*)" button for "(.*)"$/)do |button, slug|
   Capybara.ignore_hidden_elements = false
   page = DynamicPage.find_by(slug: slug)
-  find('td', text: 'Delete',visible:false).click_link('Delete')
+  find_button(id: page.slug).click
   Capybara.ignore_hidden_elements = true
-end
-Given(/^I enter (?:my)? "(.*)" as "(.*)"$/) do |field_name, input|
-  fill_in(field_name, with: input)
 end
