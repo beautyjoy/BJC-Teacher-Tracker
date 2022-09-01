6138db52456c60739af5c879b0c25d3db56b3bea
Michael Ball <cycomachead@gmail.com>
2022-09-01 16:57:08 -0700

    Do not store access/refresh tokens on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,17 +11,9 @@ class SessionsController < ApplicationController
   end
 
   def omniauth_callback
-    # Get access tokens from the server
     access_token = request.env["omniauth.auth"]
     if Teacher.validate_access_token(access_token)
-      # Tell them to register.
       user = Teacher.user_from_omniauth(access_token)
-      # Access_token is used to authenticate request made from the Rails application to the server
-      token = access_token.credentials.token
-      # Refresh_token to request new access_token
-      # Note: Refresh_token is only sent once during the first request
-      refresh_token = access_token.credentials.refresh_token
-      setTokens(token, refresh_token, user)
       user.last_session_at = Time.zone.now
       user.save!
       log_in(user)
@@ -33,23 +25,5 @@ class SessionsController < ApplicationController
 
   def omniauth_failure
     redirect_to root_url, alert: "Login failed"
-  end
-
-  private
-  def setTokens(token, refresh_token, user)
-    case params[:provider]
-    when "google_oauth2"
-      user.google_token = token
-      user.google_refresh_token = refresh_token || user.google_refresh_token
-    when "microsoft_graph"
-      user.microsoft_token = token
-      user.microsoft_refresh_token = refresh_token || user.microsoft_refresh_token
-    when "discourse"
-      user.snap_token = token
-      user.snap_refresh_token = refresh_token || user.snap_refresh_token
-    when "clever"
-      user.clever_token = token
-      user.clever_refresh_token = refresh_token || user.clever_refresh_token
-    end
   end
 end
