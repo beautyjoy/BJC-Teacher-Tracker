81694d73b354f36f313418129ae66e995a15f75b
Michael Ball <cycomachead@gmail.com>
2022-09-06 12:14:16 -0700

    fix migration name

--- db/migrate/20220905210128_rename_slug_to_url_slug.rb
+++ db/migrate/20220905210128_rename_slug_to_url_slug.rb
@@ -1,4 +1,4 @@
-class RenameSlugToUrlSlug < ActiveRecord::Migration[6.1]
+class RenameSlugToURLSlug < ActiveRecord::Migration[6.1]
   def change
     rename_column :pages, :slug, :url_slug
     rename_column :pages, :permissions, :viewer_permissions
