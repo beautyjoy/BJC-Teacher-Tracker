928183d9c80af9e8a131d67774b8a3471107f916
Michael Ball <cycomachead@gmail.com>
2022-09-05 19:11:03 -0700

    replace to text referring to renamed columns

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe PagesController, type: :controller do
     @pages_slug = "test_slug_1"
     @pages_title = "Test Page Title 1"
     @fail_flash_alert = /Failed to submit information :\(/
-    @slug_exists_flash_alert = "An error occurred! [\"Slug has already been taken\"]"
+    @slug_exists_flash_alert = "An error occurred! [\"URL Slug has already been taken\"]"
     @success_flash_alert = Regexp.new("Created #{@pages_title} page successfully.")
   end
 
@@ -147,7 +147,7 @@ RSpec.describe PagesController, type: :controller do
           user_id: 0
         }
       }
-      expect(@slug_exists_flash_alert).to match flash[:alert]
+      expect(flash[:alert]).to cotain @slug_exists_flash_alert
     end
   end
 
