66a669b1b4c86aaab061c38af73932b43755a7fa
Michael Ball <cycomachead@gmail.com>
2023-01-26 18:50:56 -0800

    delint

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -11,10 +11,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :clever, ENV["CLEVER_CLIENT_ID"], ENV["CLEVER_CLIENT_SECRET"]
 
-  provider :yahoo_oauth2, ENV['YAHOO_CLIENT_ID'], ENV['YAHOO_CLIENT_SECRET'], name: 'yahoo'
+  provider :yahoo_oauth2, ENV["YAHOO_CLIENT_ID"], ENV["YAHOO_CLIENT_SECRET"], name: "yahoo"
 end
 
 OmniAuth.config.on_failure = Proc.new do |env|
   SessionsController.action(:omniauth_failure).call(env)
-  Rails.logger.warn "Omniauth Failure: #{env.to_s}"
+  Rails.logger.warn "Omniauth Failure: #{env}"
 end
