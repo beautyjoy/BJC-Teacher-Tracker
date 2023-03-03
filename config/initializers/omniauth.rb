bdcc772b44f7201eab5be0ad088c9f300905df64
Michael Ball <cycomachead@gmail.com>
2023-03-02 22:41:22 -0800

    cleanup microsoft scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer if Rails.env.development?
+  provider :developer, fields: [:email] if Rails.env.development?
 
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
 
-  provider :microsoft_graph, ENV["MICROSOFT_CLIENT_ID"], ENV["MICROSOFT_CLIENT_SECRET"], { scope: ENV["MICROSOFT_SCOPES"] }
+  provider :microsoft_graph, ENV["MICROSOFT_CLIENT_ID"], ENV["MICROSOFT_CLIENT_SECRET"],
+           { scope: "openid profile email" }
 
   provider :discourse, sso_url: ENV["SNAP_CLIENT_URL"], sso_secret: ENV["SNAP_CLIENT_SECRET"]
 
@@ -18,5 +19,3 @@ OmniAuth.config.on_failure = Proc.new do |env|
   SessionsController.action(:omniauth_failure).call(env)
   # Rails.logger.warn "Omniauth Failure: #{env}"
 end
-
-OmniAuth.config.add_camelization("oauth", "OAuth")
