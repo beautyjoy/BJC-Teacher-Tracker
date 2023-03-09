91bdd7537d15d2b50227c2fc47edde96caa9c9d6
Reimirno <long_yu@berkeley.edu>
2023-03-09 00:40:21 -0800

    chore: add rspec tests for ip address

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -40,6 +40,38 @@ RSpec.describe TeachersController, type: :controller do
     expect(Teacher.find_by(first_name: "Short")).not_to be_nil
   end
 
+  it "appends ip address when teacher update info for themselves without repetition" do
+    ApplicationController.any_instance.stub(:is_admin?).and_return(false)
+    ApplicationController.any_instance.stub(:current_user).and_return(Teacher.find_by(first_name: "Short"))
+    short_app = Teacher.find_by(first_name: "Short")
+    ip_count = short_app.ip_history.count()
+    post :update, params: { id: short_app.id, teacher: { id: short_app.id, more_info: "changed", school_id: short_app.school_id } }
+    short_app = Teacher.find_by(first_name: "Short")
+    expect(short_app.more_info).to eq "changed"
+    ip_count_now = short_app.ip_history.count()
+    expect(ip_count_now).to eq ip_count + 1
+    expect(short_app.ip_history.last).to eq request.remote_ip
+    # now we post to update again (with the same ip address) and ip count shouldn't increase
+    ip_count = ip_count_now
+    post :update, params: { id: short_app.id, teacher: { id: short_app.id, more_info: "changed again", school_id: short_app.school_id } }
+    short_app = Teacher.find_by(first_name: "Short")
+    expect(short_app.more_info).to eq "changed again"
+    ip_count_now = short_app.ip_history.count()
+    expect(ip_count_now).to eq ip_count
+    expect(short_app.ip_history.last).to eq request.remote_ip
+  end
+
+  it "doesn't appends ip address when admin updates teacher's info" do
+    ApplicationController.any_instance.stub(:is_admin?).and_return(true)
+    ApplicationController.any_instance.stub(:current_user).and_return(Teacher.find_by(first_name: "Short"))
+    short_app = Teacher.find_by(first_name: "Short")
+    ip_count = short_app.ip_history.count()
+    post :update, params: { id: short_app.id, teacher: { id: short_app.id, snap: "foobar", school_id: short_app.school_id } }
+    short_app = Teacher.find_by(first_name: "Short")
+    expect(short_app.snap).to eq "foobar"
+    expect(short_app.ip_history.count()).to eq ip_count
+  end
+
   it "doesn't allow teacher to change their snap or email" do
     ApplicationController.any_instance.stub(:require_edit_permission).and_return(true)
     ApplicationController.any_instance.stub(:is_admin?).and_return(false)
