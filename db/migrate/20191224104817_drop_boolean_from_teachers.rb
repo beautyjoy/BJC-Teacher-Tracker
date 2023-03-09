99b48706940ad0f64ea27850a7ef98afa0dbffd3
Reimirno <long_yu@berkeley.edu>
2023-03-09 00:16:06 -0800

    fix: cherry-picked out all schema changes

--- db/migrate/20191224104817_drop_boolean_from_teachers.rb
+++ db/migrate/20191224104817_drop_boolean_from_teachers.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DropBooleanFromTeachers < ActiveRecord::Migration[5.2]
+class DropBooleanFromTeachers < ActiveRecord::Migration
   def change
     remove_column :teachers, :boolean
     remove_column :teachers, :school_name
