83badf1ab7b44beda919ae37e75042169c066b35
Reimirno <long_yu@berkeley.edu>
2023-04-05 10:27:45 -0700

    fix: changed style as per request

--- app/helpers/teacher_helper.rb
+++ app/helpers/teacher_helper.rb
@@ -6,4 +6,9 @@ module TeacherHelper
     return "-" if ["N/A", "None", "", nil].include?(teacher.snap)
     link_to(teacher.snap, "#{SNAP_USER_PAGE}#{teacher.snap}", target: "_blank")
   end
+
+  def ip_history_display(teacher)
+    return "-" if teacher.ip_history.nil? || teacher.ip_history.empty?
+    teacher.ip_history.to_sentence
+  end
 end
