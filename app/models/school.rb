f4c425a0bdeb6be3f933d7bdf2109995898ff023
Michael Ball <michael@mball.co>
2023-04-19 23:43:33 -0700

    Refactor / fix Google Maps API access

--- app/models/school.rb
+++ app/models/school.rb
@@ -24,20 +24,18 @@
 #  index_schools_on_name_city_and_website  (name,city,website)
 #
 require "uri"
+
 class School < ApplicationRecord
-  validates :name, :city, :state, :website, presence: true
-  before_save :grab_lat_lng
-
-  has_many :teachers
-
-  scope :validated, -> { where("teachers_count > 0") }
-
   VALID_STATES = [ "AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FM", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MH", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI", "VA", "WA", "WV", "WI", "WY", "International"].freeze
 
   validates :state, inclusion: { in: VALID_STATES }
+  validates :name, :city, :state, :website, presence: true
   validates_format_of :website, with: /.+\..+/, on: :create
-  MAPS_API_KEY = ENV["MAPS_API_KEY"]
-  GOOGLE_MAPS = "https://maps.googleapis.com/maps/api/geocode/"
+
+  before_save :update_gps_data, if: ->{ lat.nil? || lng.nil? }
+
+  has_many :teachers
+  scope :validated, -> { where("teachers_count > 0") }
 
   enum grade_level: {
     elementary: 0,
@@ -93,29 +91,19 @@ class School < ApplicationRecord
     "#{name} (#{city}, #{state})"
   end
 
+  def update_gps_data
+    data = GoogleMaps.get_lat_lng(maps_api_location)
+    self.lat = data[:lat]
+    self.lng = data[:lng]
+  end
+
   private
   def prefix_url(url)
     return unless url
     url.match?(/^https?:/) ? url : "https://#{url}"
   end
 
-  # TODO: URL encode this.
   def maps_api_location
-    "#{self.city}+#{self.state.sub("International", "")}".sub(" ", "+")
-  end
-
-  def grab_lat_lng
-    url = "#{GOOGLE_MAPS}json?address=#{maps_api_location}&key=#{MAPS_API_KEY}"
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE # You should use VERIFY_PEER in production
-    req = Net::HTTP::Get.new(uri.request_uri)
-    res = http.request(req)
-    data = JSON.parse(res.body)
-    unless data.nil? || data["results"].empty?
-      self.lat = data["results"][0]["geometry"]["location"]["lat"]
-      self.lng = data["results"][0]["geometry"]["location"]["lng"]
-    end
+    "#{city} #{state.sub('International', '')}"
   end
 end
