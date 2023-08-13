6485c473b938dfbc3cc66d197e049d0265b5e3f1
Michael Ball <michael@mball.co>
2023-08-12 16:58:03 -1000

    WIP: Fixup post args in schools controller spec

--- spec/controllers/schools_controller_spec.rb
+++ spec/controllers/schools_controller_spec.rb
@@ -5,213 +5,198 @@ require "rails_helper"
 RSpec.describe SchoolsController, type: :controller do
   fixtures :all
 
+  let(:admin_teacher) { teachers(:admin) }
+
   before(:all) do
     @create_school_name = "University of California, Berkeley"
     @fail_flash_alert = /Failed to submit information :\(/
     @success_flash_alert = Regexp.new("Created #{@create_school_name} successfully.")
   end
 
-  it "allows admin to create" do
-    allow_any_instance_of(ApplicationController).to receive(:require_admin).and_return(true)
-    expect(School.find_by(name: @create_school_name)).to be_nil
-    post :create, {
-        params: {
-            school: {
-                name: @create_school_name,
-                city: "Berkeley",
-                state: "CA",
-                website: "www.berkeley.edu",
-                school_type: "public",
-                grade_level: "university",
-                tags: [],
-                nces_id: 123456789000
-            }
-        }
-    }
-    expect(School.find_by(name: @create_school_name)).not_to be_nil
-    expect(@success_flash_alert).to match flash[:success]
+  context "for a regular teacher" do
+    it "denies teacher to create" do
+      expect(School.find_by(name: @create_school_name)).to be_nil
+      post :create, params: {
+          school: {
+              name: @create_school_name,
+              city: "Berkeley",
+              state: "CA",
+              website: "www.berkeley.edu",
+              school_type: "public",
+              grade_level: "university",
+              tags: [],
+              nces_id: 123456789000
+          }
+      }
+      expect(School.find_by(name: @create_school_name)).to be_nil
+    end
   end
 
-  it "denies teacher to create" do
-    expect(School.find_by(name: @create_school_name)).to be_nil
-    post :create, {
-        params: {
-            school: {
-                name: @create_school_name,
-                city: "Berkeley",
-                state: "CA",
-                website: "www.berkeley.edu",
-                school_type: "public",
-                grade_level: "university",
-                tags: [],
-                nces_id: 123456789000
-            }
+  context "for an admin" do
+    before do
+      log_in(admin_teacher)
+    end
+
+    it "allows admin to create" do
+      expect(School.find_by(name: @create_school_name)).to be_nil
+      post :create, params: {
+          school: {
+              name: @create_school_name,
+              city: "Berkeley",
+              state: "CA",
+              website: "www.berkeley.edu",
+              school_type: "public",
+              grade_level: "university",
+              tags: [],
+              nces_id: 123456789000
+          }
+      }
+      expect(School.find_by(name: @create_school_name)).not_to be_nil
+      expect(@success_flash_alert).to match flash[:success]
+    end
+
+    it "requires all fields filled" do
+      expect(School.find_by(name: @create_school_name)).to be_nil
+      post :create, params: {
+        school: {
+          name: @create_school_name,
+          # missing city
+          state: "CA",
+          website: "www.berkeley.edu",
+          school_type: "public",
+          grade_level: "university",
+          tags: [],
+          nces_id: 123456789000
+        }
+      }
+      expect(School.find_by(name: @create_school_name)).to be_nil
+      expect(flash[:alert]).to match @fail_flash_alert
+
+      post :create, params: {
+        school: {
+            name: @create_school_name,
+            city: "Berkeley",
+            state: "CA",
+            # missing website
+            school_type: "public",
+            grade_level: "university",
+            tags: [],
+            nces_id: 123456789000
+        }
+      }
+      expect(School.find_by(name: @create_school_name)).to be_nil
+      expect(flash[:alert]).to match @fail_flash_alert
+
+      post :create, params: {
+        school: {
+          # missing name
+          city: "Berkeley",
+          state: "CA",
+          website: "www.berkeley.edu",
+          school_type: "public",
+          grade_level: "university",
+          tags: [],
+          nces_id: 123456789000
+        }
+      }
+      expect(School.find_by(name: @create_school_name)).to be_nil
+      expect(@fail_flash_alert).to match flash[:alert]
+    end
+
+    it "requires proper inputs for fields" do
+      expect(School.find_by(name: @create_school_name)).to be_nil
+      # Incorrect state (not chosen from enum list)
+      post :create, params: {
+        school: {
+          name: @create_school_name,
+          city: "Berkeley",
+          state: "DISTRESS",
+          website: "www.berkeley.edu",
+          school_type: "public",
+          grade_level: "university",
+          tags: [],
+          nces_id: 123456789000
+        }
+      }
+      expect(School.find_by(name: @create_school_name)).to be_nil
+      expect(@fail_flash_alert).to match flash[:alert]
+
+      post :create, params: {
+        school: {
+          name: @create_school_name,
+          city: "Berkeley",
+          state: "CA",
+          website: "wwwberkeleyedu",
+          school_type: "public",
+          grade_level: "university",
+          tags: [],
+          nces_id: 123456789000
+        }
+      }
+      expect(School.find_by(name: @create_school_name)).to be_nil
+      expect(@fail_flash_alert).to match flash[:alert]
+
+      # Incorrect school type
+      expect { post :create, params: {
+        school: {
+            name: @create_school_name,
+            city: "Berkeley",
+            state: "CA",
+            website: "www.berkeley.edu",
+            school_type: -1,
+            grade_level: "university",
+            tags: [],
+            nces_id: 123456789000
         }
     }
-    expect(School.find_by(name: @create_school_name)).to be_nil
-  end
+      }.to raise_error(ArgumentError)
+      expect(School.find_by(name: @create_school_name)).to be_nil
 
-  it "requires all fields filled" do
-    allow_any_instance_of(ApplicationController).to receive(:require_admin).and_return(true)
-    expect(School.find_by(name: @create_school_name)).to be_nil
-    post :create, {
-        params: {
-            school: {
-                name: @create_school_name,
-                # missing city
-                state: "CA",
-                website: "www.berkeley.edu",
-                school_type: "public",
-                grade_level: "university",
-                tags: [],
-                nces_id: 123456789000
-            }
-        }
-    }
-    expect(School.find_by(name: @create_school_name)).to be_nil
-    expect(flash[:alert]).to match @fail_flash_alert
+      # Incorrect grade_level
+      expect { post :create, {
+              params: {
+                  school: {
+                      name: @create_school_name,
+                      city: "Berkeley",
+                      state: "CA",
+                      website: "www.berkeley.edu",
+                      school_type: "public",
+                      grade_level: -4,
+                      tags: [],
+                      nces_id: 123456789000
+                  }
+              }
+          }
+      }.to raise_error(ArgumentError)
+      expect(School.find_by(name: @create_school_name)).to be_nil
+    end
 
-    post :create, {
-        params: {
-            school: {
-                name: @create_school_name,
-                city: "Berkeley",
-                state: "CA",
-                # missing website
-                school_type: "public",
-                grade_level: "university",
-                tags: [],
-                nces_id: 123456789000
-            }
-        }
-    }
-    expect(School.find_by(name: @create_school_name)).to be_nil
-    expect(flash[:alert]).to match @fail_flash_alert
+    it "does not create duplicate schools in the same city" do
+      expect(School.where(name: @create_school_name).count).to eq 0
 
-    post :create, {
-        params: {
-            school: {
-                # missing name
-                city: "Berkeley",
-                state: "CA",
-                website: "www.berkeley.edu",
-                school_type: "public",
-                grade_level: "university",
-                tags: [],
-                nces_id: 123456789000
-            }
+      post :create, params: {
+        school: {
+          name: @create_school_name,
+          city: "Berkeley",
+          state: "CA",
+          website: "www.berkeley.edu",
+          school_type: "public",
+          grade_level: "middle_school",
         }
-    }
-    expect(School.find_by(name: @create_school_name)).to be_nil
-    expect(@fail_flash_alert).to match flash[:alert]
-  end
+      }
+      expect(School.where(name: @create_school_name).count).to eq 1
 
-  # TODO: Separate these into individual tests
-  it "requires proper inputs for fields" do
-    allow_any_instance_of(ApplicationController).to receive(:require_admin).and_return(true)
-    expect(School.find_by(name: @create_school_name)).to be_nil
-    # Incorrect state (not chosen from enum list)
-    post :create, {
-        params: {
-            school: {
-                name: @create_school_name,
-                city: "Berkeley",
-                state: "DISTRESS",
-                website: "www.berkeley.edu",
-                school_type: "public",
-                grade_level: "university",
-                tags: [],
-                nces_id: 123456789000
-            }
+      post :create, params: {
+        school: {
+          name: @create_school_name,
+          city: "Berkeley",
+          state: "CA",
+          website: "www.berkeley.edu",
+          school_type: "public",
+          grade_level: "university",
         }
-    }
-    expect(School.find_by(name: @create_school_name)).to be_nil
-    expect(@fail_flash_alert).to match flash[:alert]
-
-    # Incorrect website format
-    post :create, {
-        params: {
-            school: {
-                name: @create_school_name,
-                city: "Berkeley",
-                state: "CA",
-                website: "wwwberkeleyedu",
-                school_type: "public",
-                grade_level: "university",
-                tags: [],
-                nces_id: 123456789000
-            }
-        }
-    }
-    expect(School.find_by(name: @create_school_name)).to be_nil
-    expect(@fail_flash_alert).to match flash[:alert]
-
-    # Incorrect school type
-    expect { post :create, {
-            params: {
-                school: {
-                    name: @create_school_name,
-                    city: "Berkeley",
-                    state: "CA",
-                    website: "www.berkeley.edu",
-                    school_type: -1,
-                    grade_level: "university",
-                    tags: [],
-                    nces_id: 123456789000
-                }
-            }
-        }
-    }.to raise_error(ArgumentError)
-    expect(School.find_by(name: @create_school_name)).to be_nil
-
-    # Incorrect grade_level
-    expect { post :create, {
-            params: {
-                school: {
-                    name: @create_school_name,
-                    city: "Berkeley",
-                    state: "CA",
-                    website: "www.berkeley.edu",
-                    school_type: "public",
-                    grade_level: -4,
-                    tags: [],
-                    nces_id: 123456789000
-                }
-            }
-        }
-    }.to raise_error(ArgumentError)
-    expect(School.find_by(name: @create_school_name)).to be_nil
-  end
-
-  it "does not create duplicate schools in the same city" do
-    allow_any_instance_of(ApplicationController).to receive(:require_admin).and_return(true)
-    expect(School.where(name: @create_school_name).count).to eq 0
-
-    post :create, {
-        params: {
-            school: {
-                name: @create_school_name,
-                city: "Berkeley",
-                state: "CA",
-                website: "www.berkeley.edu",
-                school_type: "public",
-                grade_level: "middle_school",
-            }
-        }
-    }
-    expect(School.where(name: @create_school_name).count).to eq 1
-    post :create, {
-        params: {
-            school: {
-                name: @create_school_name,
-                city: "Berkeley",
-                state: "CA",
-                website: "www.berkeley.edu",
-                school_type: "public",
-                grade_level: "university",
-            }
-        }
-    }
-    expect(School.where(name: @create_school_name).count).to eq 1
+      }
+      expect(School.where(name: @create_school_name).count).to eq 1
+    end
   end
 end
