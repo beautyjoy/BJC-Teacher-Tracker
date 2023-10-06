51ee646209d5f47c2ae7d93198667ba3ede41e7d
Michael Ball <michael@mball.co>
2023-10-06 01:34:33 -0700

    Fix the snap user page link

--- app/helpers/teacher_helper.rb
+++ app/helpers/teacher_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module TeacherHelper
-  SNAP_USER_PAGE = "https://snap.berkeley.edu/user?user="
+  SNAP_USER_PAGE = "https://snap.berkeley.edu/user?username="
   def snap_link(teacher)
     return "-" if ["N/A", "None", "", nil].include?(teacher.snap)
     link_to(teacher.snap, "#{SNAP_USER_PAGE}#{teacher.snap}", target: "_blank")
