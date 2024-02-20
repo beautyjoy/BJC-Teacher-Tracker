870491a296efabc9855353c6ace09d700afb8671
Arush Chhatrapati <arushc@berkeley.edu>
2024-02-19 16:32:14 -0800

    update backend to send to all emails specified in To field in email template

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -11,7 +11,8 @@ class TeacherMailer < ApplicationMailer
   def welcome_email(teacher)
     @teacher = teacher
     set_body
-    mail to: teacher.email_name,
+    set_recipients
+    mail to: @recipients, #ActionMailer accepts comma-separated lists of emails
          cc: CONTACT_EMAIL,
          subject: email_template.subject
   end
@@ -20,7 +21,8 @@ class TeacherMailer < ApplicationMailer
     @teacher = teacher
     @denial_reason = denial_reason
     set_body
-    mail to: @teacher.email_name,
+    set_recipients
+    mail to: @recipients,
          cc: CONTACT_EMAIL,
          subject: email_template.subject
   end
@@ -29,7 +31,8 @@ class TeacherMailer < ApplicationMailer
     @teacher = teacher
     @request_reason = request_reason
     set_body
-    mail to: @teacher.email_name,
+    set_recipients
+    mail to: @recipients,
          cc: CONTACT_EMAIL,
          subject: email_template.subject
   end
@@ -58,7 +61,14 @@ class TeacherMailer < ApplicationMailer
     @email_template ||= EmailTemplate.find_by(title: action_name.titlecase)
   end
 
+  #renders the email body with the {{parameter}} things
   def set_body
     @body = Liquid::Template.parse(email_template.body).render(liquid_assigns).html_safe
   end
+
+  #renders the list of recipients with the {{parameter}} things
+  def set_recipients
+    @recipients = Liquid::Template.parse(email_template.to).render(liquid_assigns).html_safe
+  end
+
 end
