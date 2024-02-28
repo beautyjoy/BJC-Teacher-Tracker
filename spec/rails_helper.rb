2f30dc1b858f763e662d1c801645a5e2465c2fa8
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-02-28 13:11:50 -0800

    Revert "Add RSpec db check to ensure tests use bjc_teachers_test for safety"
    This reverts commit 9fb64e9b70c9f3f8e00bc39f887065f62afdf50c.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,8 +16,6 @@ require "support/factory_bot"
 
 require "spec_helper"
 
-require "support/db_verification"
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
