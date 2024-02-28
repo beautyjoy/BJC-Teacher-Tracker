755a96bd73f93b992ca8353113445b3f909f0bc0
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-02-28 13:23:08 -0800

    Update email reseed implementation to avoid purge old email templates

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,11 @@
 
 require_relative "seed_data"
 
-EmailTemplate.delete_all
-SeedData.emails.each { |email| EmailTemplate.create!(email) }
+SeedData.emails.each do |email_attrs|
+  # to, body fields are updated through development, should not be used as part of the uniqueness check
+  email_template = EmailTemplate.find_or_initialize_by(email_attrs.except(:to, :body))
+  email_template.update(email_attrs)
+end
 
 SeedData.create_schools
 
