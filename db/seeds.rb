60a60b5ed97936888e41b97bfe96a19d4c6dea3b
JacksonXu33 <jacksonxu@berkeley.edu>
2024-03-06 20:56:15 -0800

    fix some basic styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,7 @@
 require_relative "seed_data"
 
 SeedData.emails.each do |email_attrs|
-  # Based on what Professor Ball mentioned, 'title' is the unique identifier for each EmailTemplate
-  unique_identifier = { title: email_attrs[:title] }
-  email_template = EmailTemplate.find_or_initialize_by(unique_identifier)
+  email_template = EmailTemplate.find_or_initialize_by({ title: email_attrs[:title] })
   email_template.update(email_attrs)
 end
 
