81c4ca03d2d57a8dea98fa5e967bb2128b1e7134
Michael Ball <cycomachead@gmail.com>
2024-03-11 09:59:45 -0700

    Tidy Email templates controller

--- app/controllers/email_templates_controller.rb
+++ app/controllers/email_templates_controller.rb
@@ -18,8 +18,8 @@ class EmailTemplatesController < ApplicationController
       flash[:success] = "Updated #{template.title} template successfully."
       redirect_to email_templates_path
     else
-      flash[:alert] = "Failed to save #{template.title} template: " + template.errors.full_messages.join(", ")
-      redirect_to edit_email_template_path(params[:id])
+      flash[:alert] = "Failed to save #{template.title} template: #{template.errors.full_messages.join(", ")}" 
+      render 'edit'
     end
   end
 
@@ -36,7 +36,7 @@ class EmailTemplatesController < ApplicationController
       flash[:success] = "Created #{@email_template.title} successfully."
       redirect_to email_templates_path
     else
-      flash[:alert] = "Failed to submit information: " + @email_template.errors.full_messages.join(", ")
+      flash[:alert] = "Failed to submit information: #{@email_template.errors.full_messages.join(", ")}"
       render "new"
     end
   end
