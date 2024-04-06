8b711cc372a929ceee28fb6725a168c85fba17ab
Arush Chhatrapati <65872167+ArushC@users.noreply.github.com>
2024-04-05 18:59:43 -0700

    fixed bug in merge

--- app/controllers/merge_controller.rb
+++ app/controllers/merge_controller.rb
@@ -3,24 +3,18 @@ class MergeController < ApplicationController
     @teacher = Teacher.find(params[:id])
     @merge_teacher = Teacher.find(params[:merge_id])
     @result_teacher = merge_teachers(@teacher, @merge_teacher) 
-    # Add any logic needed for previewing the merge
   end
 
   def merge
     @teacher = Teacher.find(params[:id])
     @merge_teacher = Teacher.find(params[:merge_id])
     @result_teacher = merge_teachers(@teacher, @merge_teacher) 
-    # Add logic to merge teachers
+    #delete old teachers before saving the new merged teacher to prevent
+    #the unique email validation from failing
     @merge_teacher.destroy
+    @teacher.destroy
     @result_teacher.save!
     redirect_to teachers_path, notice: 'Teachers merged successfully.'
-    #if @merge_teacher.destroy && @result_teacher.save
-      #redirect_to teachers_path, notice: 'Teachers merged successfully.'
-    #else
-    #  @teacher_1 = Teacher.new(@teacher_1.attributes.except('id', 'created_at', 'updated_at'))
-    #  @teacher_1.save!
-    #  render :preview, alert: 'Failed to merge teachers.'
-    #end
   end
 
   private
@@ -30,7 +24,7 @@ class MergeController < ApplicationController
     merged_attributes = {}
   
     original.attributes.each do |attr_name, attr_value|
-      merged_attributes[attr_name] = attr_value.nil? ? to_merge.attributes[attr_name] : attr_value
+      merged_attributes[attr_name] = attr_value.blank? ? to_merge.attributes[attr_name] : attr_value
     end
 
     merged_teacher = Teacher.new(merged_attributes)
