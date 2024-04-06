00d4f4ba6e75257e2736f5271eb280f65f37d947
Arush Chhatrapati <65872167+ArushC@users.noreply.github.com>
2024-04-05 17:59:09 -0700

    wrote merge function and updated preview frontend accordingly

--- app/controllers/merge_controller.rb
+++ app/controllers/merge_controller.rb
@@ -2,6 +2,7 @@ class MergeController < ApplicationController
   def preview
     @teacher = Teacher.find(params[:id])
     @merge_teacher = Teacher.find(params[:merge_id])
+    @result_teacher = merge_teachers(@teacher, @merge_teacher) 
     # Add any logic needed for previewing the merge
   end
 
@@ -15,4 +16,19 @@ class MergeController < ApplicationController
       render :preview, alert: 'Failed to merge teachers.'
     end
   end
+
+  private
+  #Returns a merged teacher without saving it to the database. 
+  #Rendered in the preview, and only saved to the DB in a call to merge.
+  def merge_teachers(original, to_merge)
+    merged_attributes = {}
+  
+    original.attributes.each do |attr_name, attr_value|
+      merged_attributes[attr_name] = attr_value.nil? ? to_merge.attributes[attr_name] : attr_value
+    end
+
+    merged_teacher = Teacher.new(merged_attributes)
+    merged_teacher
+  end
+
 end
