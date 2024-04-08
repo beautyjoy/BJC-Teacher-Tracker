8f7d9ec6d2f0b9a2fa18574720fa5fd74908fccd
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-04-07 20:50:19 -0700

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,12 @@ end
 SeedData.create_schools
 
 SeedData.teachers.each do |teacher|
-  Teacher.find_or_create_by(email: teacher[:email]).update(teacher)
+  teacher_id = EmailAddress.find_by(email: teacher[:email])&.teacher_id
+  # Seed data includes email field for comparison purposes, but it is not part of the Teacher model
+  teacher_attrs = teacher.except(:email)
+  Teacher.find_or_create_by(id: teacher_id).update(teacher_attrs)
+end
+
+SeedData.email_addresses.each do |email_address|
+  EmailAddress.find_or_create_by(email: email_address[:email]).update(email_address)
 end
