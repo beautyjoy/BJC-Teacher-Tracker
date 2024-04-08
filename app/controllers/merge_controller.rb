37f7c33a1aa300625d94fce5484c492969a2a22d
razztech <razvan.turcu01@gmail.com>
2024-04-08 15:43:19 -0700

    Fixed rubocop

--- app/controllers/merge_controller.rb
+++ app/controllers/merge_controller.rb
@@ -7,14 +7,14 @@ class MergeController < ApplicationController
     @into_teacher = Teacher.find(params[:into])
     puts "Here we go"
     puts @from_teacher
-    @result_teacher = merge_teachers(@from_teacher, @into_teacher) 
+    @result_teacher = merge_teachers(@from_teacher, @into_teacher)
   end
 
   def execute
     puts params.inspect
     @from_teacher = Teacher.find(params[:from])
     @into_teacher = Teacher.find(params[:into])
-    @merged_teacher = merge_teachers(@from_teacher, @into_teacher) 
+    @merged_teacher = merge_teachers(@from_teacher, @into_teacher)
 
     merged_attributes = @merged_teacher.attributes.except("id")
     @into_teacher.update!(merged_attributes)
@@ -23,8 +23,8 @@ class MergeController < ApplicationController
   end
 
   private
-  #Returns a merged teacher without saving it to the database. 
-  #Rendered in the preview, and only saved to the DB in a call to merge.
+  # Returns a merged teacher without saving it to the database.
+  # Rendered in the preview, and only saved to the DB in a call to merge.
   def merge_teachers(from_teacher, into_teacher)
     merged_attributes = {}
     into_teacher.attributes.each do |attr_name, attr_value|
@@ -45,11 +45,10 @@ class MergeController < ApplicationController
         when "created_at"
           # The resulting record creation time is the least recent one
           merged_attributes[attr_name] = attr_value < from_teacher_attr_value ? attr_value : from_teacher_attr_value
-        else 
+        else
           merged_attributes[attr_name] = attr_value
         end
       end
-      
     end
 
     merged_teacher = Teacher.new(merged_attributes)
