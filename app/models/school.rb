f41d0b94367877c204c3a4840e646fce02251e9c
Arush Chhatrapati <65872167+ArushC@users.noreply.github.com>
2024-04-12 18:01:30 -0700

    converted valid states list using country gem

--- app/models/school.rb
+++ app/models/school.rb
@@ -26,8 +26,7 @@
 #
 
 class School < ApplicationRecord
-  VALID_STATES = [ "AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FM", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MH", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI", "VA", "WA", "WV", "WI", "WY"].freeze
-
+  VALID_STATES = ISO3166::Country['US'].subdivisions.keys.freeze
   validates :name, :city, :website, :country, presence: true
   validates :country, inclusion: { in: ISO3166::Country.all.map(&:alpha2), message: "%{value} is not a valid country" }
   validates :state, presence: true, if: -> { country == "US" }
