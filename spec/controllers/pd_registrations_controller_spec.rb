417adeaa03b481c1945924494a65caf05fe3bcf4
Jingchao Zhong <92573736+perryzjc@users.noreply.github.com>
2024-04-19 11:55:17 -0700

    Add more rspec tests

--- spec/controllers/pd_registrations_controller_spec.rb
+++ spec/controllers/pd_registrations_controller_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe "PdRegistrations", type: :request do
   let(:valid_registration_attributes) {
     {
       teacher_id: teacher1.id,
-      professional_development_id: professional_development.id,
       attended: true,
       role: "attendee"
     }
@@ -21,7 +20,6 @@ RSpec.describe "PdRegistrations", type: :request do
   let(:valid_registration_attributes2) {
     {
       teacher_id: teacher2.id,
-      professional_development_id: professional_development.id,
       attended: true,
       role: "attendee"
     }
@@ -29,91 +27,111 @@ RSpec.describe "PdRegistrations", type: :request do
   let(:invalid_registration_attributes) {
     {
       teacher_id: nil,
-      professional_development_id: nil,
       attended: false,
       role: ""
     }
   }
-  let!(:pd_registration) { PdRegistration.create!(valid_registration_attributes) }
+  let!(:pd_registration) { PdRegistration.create!(valid_registration_attributes.merge(professional_development_id: professional_development.id)) }
 
-  shared_examples "admin access" do
-    it "allows operation" do
-      action
-      expect(flash[:notice]).to match(/successfully/)
+  describe "Authorization" do
+    shared_examples "admin access" do
+      it "allows operation for admin" do
+        log_in(admin_user)
+        action
+        expect(response).to have_http_status(:redirect)
+        expect(flash[:notice]).to match(/successfully/)
+      end
     end
-  end
 
-  shared_examples "regular user denied" do
-    it "denies operation" do
-      action
-      expect(flash[:danger]).to be_present
+    shared_examples "unauthorized access" do
+      it "denies operation for regular user" do
+        log_in(regular_user)
+        action
+        expect(response).to redirect_to(root_path)
+        expect(flash[:danger]).to be_present
+      end
     end
-  end
 
-  describe "CRUD operations for PD Registrations" do
     describe "CREATE" do
-      let(:action) { post professional_development_pd_registrations_path(
-                          professional_development_id: professional_development.id),
-                          params: { pd_registration: valid_registration_attributes2 } }
+      let(:action) { post professional_development_pd_registrations_path(professional_development), params: { pd_registration: valid_registration_attributes2 } }
+      include_examples "admin access"
+      include_examples "unauthorized access"
+    end
 
-      context "as an admin" do
-        before { log_in(admin_user) }
-        include_examples "admin access"
+    describe "UPDATE" do
+      let(:action) { patch professional_development_pd_registration_path(professional_development, pd_registration), params: { pd_registration: { role: "leader" } } }
+      include_examples "admin access"
+      include_examples "unauthorized access"
+    end
+
+    describe "DELETE" do
+      let(:action) { delete professional_development_pd_registration_path(professional_development, pd_registration) }
+      include_examples "admin access"
+      include_examples "unauthorized access"
+    end
+  end
+
+  describe "Admin operations" do
+    before { log_in(admin_user) }
+
+    describe "CREATE" do
+      context "with valid attributes" do
+        it "creates a new registration successfully" do
+          expect {
+            post professional_development_pd_registrations_path(professional_development), params: { pd_registration: valid_registration_attributes2 }
+          }.to change(PdRegistration, :count).by(1)
+          expect(response).to redirect_to(professional_development_path(professional_development))
+          expect(flash[:notice]).to match(/successfully created/)
+        end
       end
 
-      context "as a regular user" do
-        before { log_in(regular_user) }
-        include_examples "regular user denied"
+      context "with invalid attributes" do
+        it "fails to create and renders errors" do
+          post professional_development_pd_registrations_path(professional_development), params: { pd_registration: invalid_registration_attributes }
+          expect(response).to render_template("professional_developments/show")
+          expect(flash.now[:alert]).to be_present
+        end
       end
     end
 
     describe "UPDATE" do
       context "with valid attributes" do
-        let(:action) { patch professional_development_pd_registration_path(
-                               professional_development_id: professional_development.id, id: pd_registration.id),
-                               params: { pd_registration: { role: "leader" } } }
-
-        context "as an admin" do
-          before { log_in(admin_user) }
-          include_examples "admin access"
-        end
-
-        context "as a regular user" do
-          before { log_in(regular_user) }
-          include_examples "regular user denied"
+        it "updates the registration successfully" do
+          patch professional_development_pd_registration_path(professional_development, pd_registration), params: { pd_registration: { role: "leader" } }
+          expect(response).to redirect_to(professional_development_path(professional_development))
+          expect(flash[:notice]).to match(/successfully updated/)
         end
       end
 
       context "with invalid attributes" do
-        let(:action) { patch professional_development_pd_registration_path(
-                                professional_development_id: professional_development.id, id: pd_registration.id),
-                                params: { pd_registration: invalid_registration_attributes } }
-
-        it "fails to update and redirects for admin" do
-          log_in(admin_user)
-          action
-          expect(response).to render_template(:show)
-          expect(flash[:alert]).to be_present
+        it "fails to update and renders errors" do
+          patch professional_development_pd_registration_path(professional_development, pd_registration), params: { pd_registration: invalid_registration_attributes }
+          expect(response).to render_template("professional_developments/show")
+          expect(flash.now[:alert]).to include("Teacher must exist and Role  is not a valid role")
         end
       end
     end
 
     describe "DELETE" do
-      let(:action) { delete professional_development_pd_registration_path(
-                          professional_development_id: professional_development.id, id: pd_registration.id) }
-
-      context "as an admin" do
-        before { log_in(admin_user) }
-        it "deletes the PD registration" do
-          expect { action }.to change(PdRegistration, :count).by(-1)
+      context "when deletion is successful" do
+        it "deletes the registration successfully and redirects" do
+          expect {
+            delete professional_development_pd_registration_path(professional_development, pd_registration)
+          }.to change(PdRegistration, :count).by(-1)
           expect(response).to redirect_to(professional_development_path(professional_development))
           expect(flash[:notice]).to match(/successfully cancelled/)
         end
       end
 
-      context "as a regular user" do
-        before { log_in(regular_user) }
-        include_examples "regular user denied"
+      context "when attempting to delete a non-existent registration" do
+        it "does not change the count of registrations and redirects with an error" do
+          expect {
+            # using -1 to simulate a non-existent ID
+            delete professional_development_pd_registration_path(professional_development, -1)
+          }.not_to change(PdRegistration, :count)
+          expect(response).to redirect_to(professional_development_path)
+          expect(flash[:alert]).to match("Registration not found.")
+        end
       end
     end
   end
