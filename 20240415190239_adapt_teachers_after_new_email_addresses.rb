65e0dd34a2bcd219dd3a040a04cd61168b7fd988
Michael Ball <michael@mball.co>
2024-06-28 13:50:34 -0700

    Run rubocop

--- 20240415190239_adapt_teachers_after_new_email_addresses.rb
+++ 20240415190239_adapt_teachers_after_new_email_addresses.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AdaptTeachersAfterNewEmailAddresses < ActiveRecord::Migration[6.1]
   def change
     remove_index :teachers, name: "index_teachers_on_email_and_first_name"
