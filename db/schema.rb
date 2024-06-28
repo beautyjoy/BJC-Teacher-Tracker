1760b63a34b0fbd67bbfc6db5d1ad89cb92f4e01
Michael Ball <michael@mball.co>
2024-06-28 12:51:25 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2023_09_05_181019) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
