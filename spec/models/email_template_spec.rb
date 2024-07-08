bf0e16724f5d74d0c012ac9f9ca341c9a1918faf
Michael Ball <michael@mball.co>
2024-07-08 13:59:29 -0700

    Add some basic email specs

---
+++ spec/models/email_template_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe EmailTemplate, type: :model do
+  describe '#reason?' do
+    it 'returns true if a reason is present in the body' do
+      template = EmailTemplate.new(body: ' ... {{ denial_reason }}...')
+      expect(template.reason?).to eq(true)
+    end
+
+    it 'returns true even if there are other liquid filters' do
+      template = EmailTemplate.new(body: ' ... {{denial_reason|strip_tags}}...')
+      expect(template.reason?).to eq(true)
+    end
+
+    it 'returns false if no reason is in the body' do
+      template = EmailTemplate.new
+      expect(template.reason?).to eq(false)
+    end
+  end
+end
