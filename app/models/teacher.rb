67aad210c6a4fd607ddf99fa8cd9598d2232fe87
Michael Ball <michael@mball.co>
2024-07-22 12:29:53 -0700

    clarity on blank values in form fields

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -274,7 +274,6 @@ class Teacher < ApplicationRecord
       # TODO: change to personal_emails
       teacher_personal_email: self.non_primary_emails.join(', '),
       teacher_more_info: self.more_info,
-      teacher_snap: self.snap_username,
       teacher_snap_username: self.snap_username,
       teacher_education_level: self.education_level,
       teacher_personal_website: self.personal_website,
@@ -286,7 +285,7 @@ class Teacher < ApplicationRecord
       teacher_school_city: self.school.city,
       teacher_school_state: self.school.state,
       teacher_school_website: self.school.website,
-    }
+    }.trasform_values { |value| value.blank? ? '(blank)' value }
   end
 
   # TODO: The school data needs to be cleaned up.
