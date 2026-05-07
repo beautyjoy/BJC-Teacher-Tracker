29c3ed78b47f6a649fd7207e7e6cb60dc46381c7
Kien Huynh <143785443+kienthuynh@users.noreply.github.com>
2026-05-06 18:08:21 -0700

    add seeding script

--- db/seed_data.rb
+++ db/seed_data.rb
@@ -234,7 +234,7 @@ module SeedData
             # Include it in the seed data is to simulate the behavior of creating a new teacher,
             # because we need to use it to compared with the EmailAddress model,
             # to determine the existence of the teacher
-            email: "ball@berkeley.edu",
+            email: "mball@berkeley.edu",
         },
         {
             first_name: "Lauren",
@@ -419,7 +419,7 @@ module SeedData
   def self.email_addresses
     [
       {
-        email: "ball@berkeley.edu",
+        email: "mball@berkeley.edu",
         primary: true,
         teacher: Teacher.find_by(first_name: "Michael"),
       },
